c0e965f064dc0fdbfe59bf4208bcce3e0b9c6b1f
KANGDONGHOON0413 <kmerits@gmail.com>
2022-09-24 20:32:18 +0900

    2일차 업데이트

--- WebApplication7/Controllers/ManageUsersController.cs
+++ WebApplication7/Controllers/ManageUsersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApplication7.DataContext;
 using WebApplication7.Models;
+using WebApplication7.ViewModels;
 
 namespace WebApplication7.Controllers
 {
@@ -14,12 +15,18 @@ namespace WebApplication7.Controllers
         {
             return View();
         }
+
+
+
+        #region 회원가입
         //default값은 get방식이다
         //html값을 받아오는 것은 get방식으로 한다
         public IActionResult Register()
         {
             return View();
         }
+
+
         [HttpPost]
         public IActionResult Register(User model)
         {
@@ -34,10 +41,37 @@ namespace WebApplication7.Controllers
             
             return View(model);
         }
+        #endregion
 
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginCheck model)
+        {
+            if (ModelState.IsValid)
+            {
+                using(var db = new AspnetDBContext())
+                {
+                    //Linq-메서드 체이닝 식(메서드 뒤에 메서드를 다는것) FirstOrDefault 은
+                    //하나만 검색할 때 쓰는 방식으로, 처음값 또는 디폴트값만을 검색
+                    //var user = db.Users.FirstOrDefault(A => A.UserID == model.UserID && A.UserPW == model.UserPW); ->메모리를 비효율적으로 사용하게 됨
+                    var user = db.Users.FirstOrDefault(A => A.UserID.Equals(model.UserID) && A.UserPW.Equals(model.UserPW));
+                    if (!(user is null))
+                    {
+                        return RedirectToAction("LoginSuccess", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "사용자 정보가 올바르지 않습니다");
+                        return View();
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
